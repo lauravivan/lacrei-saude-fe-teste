8ffb4b88a81e9b9cc97ec3e5983bdb12cc4f655d
lauravivan <lauvivang@gmail.com>
2025-10-01 21:52:45 -0300

    Add help svg and styles to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,42 @@
-export default function Header() {
-  return <header></header>;
+import Image from "next/image";
+import styled from "styled-components";
+import LogoHeader from "@/assets/img/logo/logo-header-mobile.svg";
+import { useScreenSize } from "@/hooks/useScreenSize";
+import HelpButton from "./button/HelpButton";
+
+const Header = styled.header`
+  background-image: var(--gradient-subtle);
+  display: flex;
+  align-items: center;
+  padding: var(--spacing-m);
+  justify-content: between;
+
+  .img-wrapper {
+    width: 134px;
+    height: 16px;
+  }
+`;
+
+export default function HeaderComponent() {
+  const { isDesktop } = useScreenSize();
+
+  return (
+    <Header>
+      <div className="img-wrapper">
+        {isDesktop ? (
+          <div></div>
+        ) : (
+          <Image
+            src={LogoHeader}
+            alt="Logo Lacrei Saúde"
+            width={134}
+            height={16}
+          />
+        )}
+      </div>
+      <div>
+        <HelpButton />
+      </div>
+    </Header>
+  );
 }
---
+++ src/components/button/HelpButton.tsx
@@ -0,0 +1,9 @@
+import Help from "../icons/Help";
+
+export default function HelpButton() {
+  return (
+    <a>
+      <Help />
+    </a>
+  );
+}
