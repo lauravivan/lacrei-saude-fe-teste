4b1c697f441090b11adbb1eea4dadbc4f201cc78
lauravivan <lauvivang@gmail.com>
2025-10-02 10:19:52 -0300

    Add query client

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,16 +5,24 @@ import Header from "@/components/Header";
 import { ThemeProvider } from "styled-components";
 import { theme } from "@/theme";
 import GlobalStyles from "@/GlobalStyles";
+import { Suspense } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App({ children }: { children?: React.ReactNode }) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <div id="app">
-        <Header />
-        {children}
-        <Footer />
-      </div>
-    </ThemeProvider>
+    <Suspense>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <div id="app">
+            <Header />
+            {children}
+            <Footer />
+          </div>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </Suspense>
   );
 }
