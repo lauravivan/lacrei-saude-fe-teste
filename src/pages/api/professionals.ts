8727f511fe14cdda4ac1e3a5d21e36fb1aa769bf
lauravivan <lauvivang@gmail.com>
2025-10-03 15:01:10 -0300

    Move api folder to src

--- src/pages/api/professionals.ts
+++ src/pages/api/professionals.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { promises as fs } from "fs";
 
 export default async function handler(req, res) {
-  const filePath = path.join(process.cwd(), "api", "data.json");
+  const filePath = path.join(process.cwd(), "src", "api", "data.json");
   const jsonData = await fs.readFile(filePath, "utf8");
   const data = JSON.parse(jsonData);
 
