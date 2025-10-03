a868311edd4d2877fa695fca4ce43ad9b71bc0e3
lauravivan <lauvivang@gmail.com>
2025-10-03 02:51:45 -0300

    Add professional card

---
+++ src/app/profissionais/_components/_cards/Professional.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import {
+  BadgeEthnicity,
+  BadgeGenderIdentity,
+  BadgeSexuality,
+} from "@/components/badge/BadgeDiversity";
+import Title from "@/components/Title";
+import { Professional as ProfessionalType } from "@/types/professional";
+import styled from "styled-components";
+import Text from "@/components/Text";
+
+type ProfessionalsType = {
+  professional: ProfessionalType;
+};
+
+const ProfessionalCard = styled.article`
+  display: flex;
+  padding: var(--spacing-inset-m) var(--spacing-inset-xm);
+  column-gap: 0 var(--spacing-inset-m);
+
+  > div:nth-child(2) {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  .professional__identity {
+    display: flex;
+    column-gap: var(--spacing-inline-xs);
+    margin-bottom: var(--spacing-inline-m);
+  }
+
+  .professional__services {
+    margin-left: auto;
+  }
+`;
+
+export default function Professional({ professional }: ProfessionalsType) {
+  return (
+    <ProfessionalCard>
+      <div>
+        <img />
+      </div>
+      <div>
+        <div>
+          <Title number={5}>{professional.name}</Title>
+          <Text variant="Text-sm-high200">
+            {`${professional.specialty}, ${professional.crm}`}
+          </Text>
+          <div className="professional__identity">
+            <BadgeEthnicity label={professional.identity.race} />
+            <BadgeGenderIdentity label={professional.identity.gender} />
+            <BadgeSexuality label={professional.identity.sexuality} />
+          </div>
+          <div></div>
+          <details>
+            <summary>Informações</summary>
+            <Text variant="Text-base">{professional.bio}</Text>
+          </details>
+        </div>
+        <div className="professional__services">
+          <button>Atendimentos</button>
+        </div>
+      </div>
+    </ProfessionalCard>
+  );
+}
