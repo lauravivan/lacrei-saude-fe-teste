37bdc28ca16402feb631cda1d6e3f6c52a5b6e44
lauravivan <lauvivang@gmail.com>
2025-10-08 14:05:38 -0300

    chore: add commitlint rules and and instructions in commit-msg

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,12 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-case": [2, "always", "lower-case"],
+    "scope-case": [2, "always", "lower-case"],
+    "subject-case": [2, "always", "lower-case"],
+    "subject-full-stop": [2, "never", "."],
+    "subject-empty": [2, "never"],
+    "type-empty": [2, "never"],
+    "header-max-length": [2, "always", 72],
+  },
+};
