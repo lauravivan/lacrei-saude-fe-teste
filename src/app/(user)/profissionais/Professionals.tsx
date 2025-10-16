e68451c7b707f786a3ccd55b72dca417337d06d3
lauravivan <lauvivang@gmail.com>
2025-10-16 01:28:47 -0300

    refactor: add separate file for professionals styles and remove className

--- src/app/(user)/profissionais/Professionals.tsx
+++ src/app/(user)/profissionais/Professionals.tsx
@@ -4,32 +4,16 @@ import Professional from "./_components/cards/Professional";
 import { useQuery } from "@tanstack/react-query";
 import { getProfessionals } from "./_routes/getProfessionals";
 import { Professional as ProfessionalType } from "@/types/professional";
-import styled from "styled-components";
 import HeaderSignOut from "@/components/header/header-sign-out/HeaderSignOut";
 import Footer from "@/components/footer/Footer";
-import spacing from "@/styles/tokens/spacing";
 import SkipLink from "@/components/skip-link/SkipLink";
+import {
+  ProfessionalsContainer,
+  ProfessionalsResults,
+  Text,
+} from "./_Professionals.styles";
 
-const ProfessionalsMain = styled.main`
-  margin: 0 auto;
-
-  h2,
-  h3,
-  > p {
-    margin: ${spacing["spacing-inset-m"]} ${spacing["spacing-inset-xm"]};
-  }
-
-  .professionals {
-    &__professionals {
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      row-gap: ${spacing["spacing-inset-xxs"]};
-    }
-  }
-`;
-
-export default function Professionals() {
+const Professionals = () => {
   const { data: professionals } = useQuery({
     queryKey: ["professionals"],
     refetchOnWindowFocus: false,
@@ -43,29 +27,23 @@ export default function Professionals() {
     <>
       <SkipLink href="#main-content">Ir para o conteúdo principal</SkipLink>
       <HeaderSignOut showSignOut={true} showPopover={true} showSearch={true} />
-      <ProfessionalsMain id="main-content">
-        {/* {professionals && (
-          <Text variant="Text-base">
-            {`Encontramos ${professionals.length} resultados`}
-          </Text>
-        )} */}
-        <div className="professionals__professionals">
+      <ProfessionalsContainer id="main-content">
+        {professionals && (
+          <Text>{`Encontramos ${professionals.length} resultados`}</Text>
+        )}
+        <ProfessionalsResults>
           {professionals && professionals.length > 0 ? (
             professionals.map((professional: ProfessionalType) => (
-              <Professional
-                key={professional.id}
-                professional={professional}
-              ></Professional>
+              <Professional key={professional.id} professional={professional} />
             ))
           ) : (
-            // <Text variant="Text-base">
-            //   Não foram encontradas pessoas profissionais
-            // </Text>
-            <div></div>
+            <Text>Não foram encontradas pessoas profissionais</Text>
           )}
-        </div>
-      </ProfessionalsMain>
+        </ProfessionalsResults>
+      </ProfessionalsContainer>
       <Footer />
     </>
   );
-}
+};
+
+export default Professionals;
---
+++ src/app/(user)/profissionais/_Professionals.styles.ts
@@ -0,0 +1,24 @@
+import text from "@/styles/mixins/text";
+import spacing from "@/styles/tokens/spacing";
+import styled from "styled-components";
+
+export const ProfessionalsContainer = styled.main`
+  margin: 0 auto;
+
+  h2,
+  h3,
+  > p {
+    margin: ${spacing["spacing-inset-m"]} ${spacing["spacing-inset-xm"]};
+  }
+`;
+
+export const Text = styled.p`
+  ${text["Text-base"]};
+`;
+
+export const ProfessionalsResults = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  row-gap: ${spacing["spacing-inset-xxs"]};
+`;
