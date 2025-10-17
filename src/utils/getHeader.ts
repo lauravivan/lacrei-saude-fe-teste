30047964562664364424b32feec2a5346fb1b096
lauravivan <lauvivang@gmail.com>
2025-10-17 00:45:23 -0300

    fix: add use server to getHeader

--- src/utils/getHeader.ts
+++ src/utils/getHeader.ts
@@ -1,8 +1,10 @@
+"use server";
+
 import { headers } from "next/headers";
 
 const getHeader = async () => {
   const head = await headers();
   return `${head.get("x-forwarded-proto") ?? "https"}://${head.get("host")}`;
 };
-    
+
 export default getHeader;
